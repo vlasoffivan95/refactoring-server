b14fdad6796be6ca92e86dabd5b4b1db7162746d
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-25 22:11:08 +0300

    refactoring login and signup button

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -7,16 +7,16 @@ import styles from './LoginPage.module.sass';
 import { clearAuthError } from '../../store/slices/authSlice';
 import CONSTANTS from '../../constants';
 
-const LoginPage = props => (
+const LoginPage = (props) => (
   <div className={styles.mainContainer}>
     <div className={styles.loginContainer}>
       <div className={styles.headerSignUpPage}>
-        <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt='logo' />
-        <div className={styles.linkLoginContainer}>
-          <Link to='/registration' style={{ textDecoration: 'none' }}>
+        <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt="logo" />
+        <Link to="/registration" style={{ textDecoration: 'none' }}>
+          <div className={styles.linkLoginContainer}>
             <span>Signup</span>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
       <div className={styles.loginFormContainer}>
         <LoginForm history={props.history} />
@@ -25,7 +25,7 @@ const LoginPage = props => (
   </div>
 );
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   clearError: () => dispatch(clearAuthError()),
 });
 
--- client/src/pages/RegistrationPage/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage/RegistrationPage.jsx
@@ -7,7 +7,7 @@ import styles from './RegistrationPage.module.sass';
 import { clearAuthError } from '../../store/slices/authSlice';
 import CONSTANTS from '../../constants';
 
-const RegistrationPage = props => {
+const RegistrationPage = (props) => {
   props.clearError();
 
   return (
@@ -15,17 +15,19 @@ const RegistrationPage = props => {
       <div className={styles.signUpContainer}>
         <div className={styles.headerSignUpPage}>
           <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} />
-          <div className={styles.linkLoginContainer}>
-            <Link to='/login' style={{ textDecoration: 'none' }}>
-              <span>Login</span>
-            </Link>
-          </div>
+
+          <Link to="/login" style={{ textDecoration: 'none' }}>
+            <div className={styles.linkLoginContainer}>
+              <span>Login</span>{' '}
+            </div>
+          </Link>
         </div>
         <RegistrationForm history={props.history} />
       </div>
       <div className={styles.footer}>
         <div className={styles.articlesMainContainer}>
           <div className={styles.ColumnContainer}>
+            s
             <div className={styles.headerArticle}>
               Why should I use Squadhelp?
             </div>
@@ -119,7 +121,7 @@ const RegistrationPage = props => {
   );
 };
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   clearError: () => dispatch(clearAuthError()),
 });
 

a15287b6c4c98e9721b4b92bb57925108484ddf0
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-25 19:25:43 +0300

    add img link

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -116,11 +116,12 @@ class Header extends React.Component {
           </div>
         </div>
         <div className={styles.navContainer}>
+         <Link to ="/">
           <img
             src={`${CONSTANTS.STATIC_IMAGES_PATH}blue-logo.png`}
             className={styles.logo}
             alt="blue_logo"
-          />
+          /></Link>
           <div className={styles.leftNav}>
             <div className={styles.nav}>
               <ul>

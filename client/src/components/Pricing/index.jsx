431440ce5acccd3e6528390bd4695906dc041ce5
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-30 00:20:53 +0300

    add map function

--- client/src/components/Pricing/index.jsx
+++ client/src/components/Pricing/index.jsx
@@ -12,8 +12,15 @@ function Pricing({
   partial_refund,
   title,
 }) {
-  console.log(styles[titleName]);
-  console.log(titleName);
+  let listBenefits;
+  if (validation_and_upgrade_list) {
+    listBenefits = validation_and_upgrade_list.map((componentList) => (
+      <li key={componentList.toString()}>{componentList}</li>
+    ));
+  } else {
+    listBenefits = null;
+  }
+
   return (
     <div>
       <section
@@ -28,10 +35,21 @@ function Pricing({
         <p className={styles.price}>US${price}</p>
       </section>
       <section className={styles.cardDescription}>
-        <p className = {styles.pDescription}>Prize to Winner - $ {prize_to_winner} (Included)</p>
-        <p className = {styles.pDescription}>{validation_and_upgrade} (Included)</p>
-        <p className = {styles.pDescription}>{expected}</p>
-        {partial_refund? <p className = {styles.pDescription} display>{partial_refund}</p>:null}
+        <p className={styles.pDescription}>
+          Prize to Winner - $ {prize_to_winner} (Included)
+        </p>
+        <p className={styles.pDescription}>
+          {validation_and_upgrade} (Included)
+        </p>
+        <p className={styles.pDescription}>{expected}</p>
+        {partial_refund ? (
+          <p className={styles.pDescription} display>
+            {partial_refund}
+          </p>
+        ) : null}
+
+        <button className={styles.btn}>✓Start</button>
+        {listBenefits}
       </section>
     </div>
   );
--- client/src/pages/PricingPage/index.jsx
+++ client/src/pages/PricingPage/index.jsx
@@ -4,14 +4,19 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import styles from './styles.module.scss'
 import STATUS from '../../data/constants';
-const {BRONZE, GOLD, MANAGED} = STATUS
+const {BRONZE, GOLD, PLATINUM, MANAGED} = STATUS
 
 const PricingPage = () => {
     return (
         <div>
             <Header />
             <div className={styles.priceContainer}>
-            <Pricing {...BRONZE}/>
+            <div className={styles.card}><Pricing {...BRONZE}/></div>
+            <div className={styles.card}><Pricing {...GOLD} /></div>
+            <div className={styles.card}><Pricing {...PLATINUM}/></div>
+            <div className={styles.card}><Pricing {...MANAGED}/></div>
+
+
             </div>
             <Footer/>
         </div>
